e38e59c6c262728cdd8dc83dd81486d6b12480ae
hwan0143@student.monash.edu <hwan0143@student.monash.edu>
2021-10-18 00:11:43 +0800

    add readme

---
+++ R/key_crop_yields.R
@@ -0,0 +1,14 @@
+#' Title
+#'
+#' @section Background:
+#' xxx The data was collected fromxxx
+#' @format a data frame with x rows and x variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#'   }
+#' @source
+#' @examples
+#' print(key_crop_yields)
+"key_crop_yields"
